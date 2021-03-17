3b6a0d0171bf838e64bd4143cc4180f4a0aec5b5
Ben Higginbotham <bicycleben88@gmail.com>
2021-03-17 11:09:01 -0400

    create custom useCart hook to open / close cart component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import { Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Items from './pages/Items'
-import Show from './pages/Show';
-import Cart from './pages/Cart';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Thanks from './pages/Thanks';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Items from "./pages/Items";
+import Show from "./pages/Show";
+// import Cart from './pages/Cart';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Thanks from "./pages/Thanks";
+import Cart from "./components/Cart";
+import { CartStateProvider } from "./lib/cartState";
 
 export const GlobalContext = React.createContext(null);
 
 const theme = {
-  lightYellow:  '#ffffcc',
-  lightBlue: '#e5f4fe',
-  boldBlue: '#5fa2cf',
-  boldYellow: '#f0f046',
-  offwhite: '#f8f8ff',
-  maxWidth: "1100px", 
-  black: '#393939', 
-  boxS: '0 12px 12px 0 #f0f046'
-}
+  lightYellow: "#ffffcc",
+  lightBlue: "#e5f4fe",
+  boldBlue: "#5fa2cf",
+  boldYellow: "#f0f046",
+  offwhite: "#f8f8ff",
+  maxWidth: "1100px",
+  black: "#393939",
+  boxS: "0 12px 12px 0 #f0f046",
+};
 
 const StyledPage = styled.div`
   main {
     background-color: lightyellow;
     height: 100%;
     margin-top: 2rem;
-    padding: .5rem;
+    padding: 0.5rem;
     min-height: 400px;
   }
   a {
-    text-decoration: none
+    text-decoration: none;
   }
 `;
 
@@ -42,42 +44,56 @@ function App() {
     itemsInOrder: false,
     orderId: null,
     userId: null,
-    token: null
+    token: null,
   });
-  
+
   return (
-    <GlobalContext.Provider value={{globalState, setGlobalState}}>
-    <ThemeProvider theme={theme}>
-      <StyledPage className="App">
-        <Header />
-        <main>
-          <Switch>
-            <Route exact path="/" 
+    <GlobalContext.Provider value={{ globalState, setGlobalState }}>
+      <ThemeProvider theme={theme}>
+        <CartStateProvider>
+          <StyledPage className="App">
+            <Header />
+            <main>
+              <Cart />
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={(rProps) => <Items {...rProps} />}
+                />
+                <Route
+                  exact
+                  path="/show"
+                  render={(rProps) => (
+                    <Show {...rProps} item={globalState.item} />
+                  )}
+                />
+                {/* <Route exact path="/cart" 
               render={rProps => 
-              <Items {...rProps} />} />
-            <Route exact path="/show" 
-              render={rProps => 
-              <Show {...rProps}  
-                item={globalState.item} />} />
-            <Route exact path="/cart" 
-              render={rProps => 
-              <Cart {...rProps}  
-                item={globalState.item} />} />
-            <Route exact path="/login"
-              render= {rProps => 
-              <Login {...rProps} />} />
-            <Route exact path="/signup"
-              render= {rProps => 
-              <Signup {...rProps} />} />
-            <Route exact path="/spanx"
-              render= {rProps => 
-              <Thanks {...rProps} />} />
-          </Switch>
-        </main>
-      </StyledPage>
-    </ThemeProvider>
+                <Cart {...rProps}  
+              item={globalState.item} />} /> */}
+                <Route
+                  exact
+                  path="/login"
+                  render={(rProps) => <Login {...rProps} />}
+                />
+                <Route
+                  exact
+                  path="/signup"
+                  render={(rProps) => <Signup {...rProps} />}
+                />
+                <Route
+                  exact
+                  path="/spanx"
+                  render={(rProps) => <Thanks {...rProps} />}
+                />
+              </Switch>
+            </main>
+          </StyledPage>
+        </CartStateProvider>
+      </ThemeProvider>
     </GlobalContext.Provider>
   );
 }
- 
+
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,35 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import NavStyles from '../components/styles/NavStyles';
-import { GlobalContext } from '../App';
-import Badge from '../components/styles/Badge';
+import React from "react";
+import { Link } from "react-router-dom";
+import NavStyles from "../components/styles/NavStyles";
+import { GlobalContext } from "../App";
+import Badge from "../components/styles/Badge";
+import { useCart } from "../lib/cartState";
 
 const Nav = (props) => {
-    const { globalState, setGlobalState } = React.useContext(GlobalContext);
-    const { itemsInOrder, token, } = globalState;
+  const { globalState, setGlobalState } = React.useContext(GlobalContext);
+  const { itemsInOrder, token } = globalState;
+  const { openCart } = useCart();
 
-    const logOut = () => {
-        return(
-            <Link 
-                to="/" 
-                onClick={() => setGlobalState({...globalState, token: null, userId: null})}>
-                Log Out
-            </Link>
-        );
-    };
-    
-    return(
-        <NavStyles>
-            <Link to="/">Items</Link>
-            <Link to="/cart">
-                Cart
-                {itemsInOrder ? <Badge><p>!</p></Badge> : null}
-            </Link>
-            {token ? logOut() : null}
-            <Link to="/login">Log In</Link>
-            <Link to="/signup">Sign Up</Link>
-        </NavStyles>
+  const logOut = () => {
+    return (
+      <Link
+        to="/"
+        onClick={() =>
+          setGlobalState({ ...globalState, token: null, userId: null })
+        }
+      >
+        Log Out
+      </Link>
     );
+  };
+
+  return (
+    <NavStyles>
+      <Link to="/">Items</Link>
+      <button onClick={openCart}>
+        Cart
+        {itemsInOrder ? (
+          <Badge>
+            <p>!</p>
+          </Badge>
+        ) : null}
+      </button>
+      {token ? logOut() : null}
+      <Link to="/login">Log In</Link>
+      <Link to="/signup">Sign Up</Link>
+    </NavStyles>
+  );
 };
 
 export default Nav;
