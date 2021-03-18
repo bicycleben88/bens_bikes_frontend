3fffdefa0335b5d74706f8092fec162184c250a3
Ben Higginbotham <bicycleben88@gmail.com>
2021-03-18 11:53:18 -0400

    style cart& cart item

--- src/components/styles/CartItemStyles.js
+++ src/components/styles/CartItemStyles.js
@@ -1,50 +1,36 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const CartItemStyles = styled.div`
+  display: flex;
+  padding: 0.5rem;
+  margin-top: 0.5rem auto;
+  width: 80%;
+  align-items: center;
+  h1 {
+    font-size: 2.5rem;
+  }
+  .cart-item-image {
+    width: 150px;
+    height: 150px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-color: ${(props) => props.theme.lightBlue};
+  }
+  .cart-item-info {
     display: flex;
-    background-color: white;
-    box-shadow: ${props => props.theme.boxS};
-    padding: .5rem;
-    margin-top: .5rem;
-    width: 600px;
-    height: 350px;
-    align-items: center;
-    h1 {
-        font-size: 2.5rem;
+    flex-direction: column;
+    align-items: flex-end;
+    height: 100%;
+    flex: 1;
+    div {
+      position: relative;
     }
-    .cart-item-image {
-        width: 250px;
-        height: 300px;
-        background-size: contain;
-        background-repeat: no-repeat;
-        background-position: center;
-        background-color: ${props => props.theme.lightBlue};
+    div:hover {
+      cursor: pointer;
+      justify-self: flex-end;
     }
-    .cart-item-info{
-        display: flex;
-        flex-direction: column;
-        align-items: flex-end;
-        height: 100%;
-        flex: 1;
-        div {
-            position: relative;
-        }
-        div:hover{
-            cursor: pointer;
-            justify-self: flex-end;
-        }
-    }
-    @media (max-width: 900px) {
-        flex-direction: column;
-        width: 300px;
-        .cart-item-image {
-            width: 200px;
-            height: 250px;
-        }
-        .cart-item-info {
-            width: 90%;
-        }
-    }
+  }
 `;
 
 export default CartItemStyles;
