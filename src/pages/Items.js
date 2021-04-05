d70aa1ce52a86347650619c0faae6d9bcd7689c7
Ben Higginbotham <bicycleben88@gmail.com>
2021-04-05 12:43:16 -0400

    create pagination component w/api to items_count

--- src/pages/Items.js
+++ src/pages/Items.js
@@ -1,46 +1,40 @@
-import React from 'react';
-import Carousel from 'react-image-carousel';
-import ItemsContainer from '../components/styles/ItemsContainer';
-import { GlobalContext } from '../App';
-import Item from '../components/Item';
+import React from "react";
+import Carousel from "react-image-carousel";
+import ItemsContainer from "../components/styles/ItemsContainer";
+import { GlobalContext } from "../App";
+import Item from "../components/Item";
+import Pagination from "../components/Pagination";
 
 const Items = (props) => {
-    const { globalState } = React.useContext(GlobalContext);
-    const { url } = globalState;
-    const [items, setItems] = React.useState([]);
-    let carouselImages = [];
+  const { globalState } = React.useContext(GlobalContext);
+  const { url } = globalState;
+  const [items, setItems] = React.useState([]);
 
-    const getItems = async () => {
-        const repsonse = await fetch (`${url}/items`);
-        const data = await repsonse.json();
-        setItems(data);
-    };
+  const getItems = async () => {
+    const repsonse = await fetch(`${url}/items`);
+    const data = await repsonse.json();
+    setItems(data);
+  };
 
-    React.useEffect(() => {
-        getItems()
-    }, []);
+  React.useEffect(() => {
+    getItems();
+  }, []);
 
-    const loaded = () => {
-        
-        items.map((item) => carouselImages.push(item.largeimage));
-        
-        return (
-            <div>
-                <ItemsContainer>
-                    {items.map((item) => <Item item={item} key={item.id}/>)}
-                    <Carousel 
-                        images={carouselImages}
-                        thumb={true}
-                        loop={true}
-                        autoplay={4000} />
-                </ItemsContainer>
-            </div>
-        );
-    };
-
+  const loaded = () => {
     return (
-        Items.length > 0 ? loaded() : <h3>getting items...</h3>
-    )
+      <div>
+        <Pagination />
+        <ItemsContainer>
+          {items.map((item) => (
+            <Item item={item} key={item.id} />
+          ))}
+        </ItemsContainer>
+        <Pagination />
+      </div>
+    );
+  };
+
+  return Items.length > 0 ? loaded() : <h3>getting items...</h3>;
 };
 
 export default Items;
