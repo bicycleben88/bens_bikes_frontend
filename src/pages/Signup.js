3c3b773c44f8a0df24dce90e4fa3d109932acd5f
Ben Higginbotham <bicycleben88@gmail.com>
2021-04-14 21:17:36 -0400

    add email to sign up form

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,28 +1,63 @@
-import React from 'react';
-import Form from '../components/Form';
-import { GlobalContext } from '../App';
+import React from "react";
+import Form from "../components/Form";
+import FormStyles from "../components/styles/FormStyles";
+import { GlobalContext } from "../App";
 
 const Signup = (props) => {
-    const { globalState } = React.useContext(GlobalContext);
-    const { url } = globalState
-    
-    const signUpUser = async (user) => {
-        await fetch(`${url}/users`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/JSON"
-            },
-            body: JSON.stringify(user)
-        });
-        props.history.push('/login');
-    };
-    
-    return(
-       <Form 
-            label="Sign Up"
-            submit={signUpUser}
-       />
-    );
+  const { globalState } = React.useContext(GlobalContext);
+  const { url } = globalState;
+  const [formData, setFormData] = React.useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  const signUpUser = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`${url}/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/JSON",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await response.json();
+    await console.log(data);
+    // props.history.push("/login");
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <FormStyles onSubmit={signUpUser}>
+      <h2>Sign Up For an Account</h2>
+
+      <input
+        name="email"
+        placeholder="email"
+        value={formData.email}
+        type="text"
+        onChange={handleChange}
+      />
+      <input
+        name="username"
+        placeholder="username"
+        value={formData.username}
+        type="text"
+        onChange={handleChange}
+      />
+      <input
+        name="password"
+        placeholder="password"
+        value={formData.password}
+        type="password"
+        onChange={handleChange}
+      />
+      <input value="Sign Up" type="submit" className="submit-button" />
+    </FormStyles>
+  );
 };
 
 export default Signup;
